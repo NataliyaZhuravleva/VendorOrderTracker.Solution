3e4863d88557084ac4a245cdf156c95cce8f8bc6
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-29 23:55:59 -0800

    Index for vendors

---
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
