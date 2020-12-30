b9d9fd957e5a645d6929ae6bd96c9e7907db0664
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-30 13:03:04 -0800

    Update new order form

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,11 +5,11 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
-    public int Id { get; }
     public string Title { get; set; }
     public string Description { get; set; }
     public float Price { get; set; }
     public string Date { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
     public Order(string title, string description, float price, string date)
