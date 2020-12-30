ba11b3497a617692ee171d486619e45c77dae848
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-29 23:45:07 -0800

    Test AssociatesOrderWithVendor purposefully fail

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -55,7 +55,7 @@ namespace VendorOrderTracker.Tests
       string description2 = "Test Vendor Description2";
       Vendor newVendor2 = new Vendor(name2, description2);
 
-      List<Vendor> newList=new List<Vendor>{newVendor1, newVendor2};
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
 
       List<Vendor> result = Vendor.GetAll();
 
@@ -65,17 +65,38 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void Find_ReturnsCorrectVendor_Vendor()
     {
-     string name1 = "Test Vendor1";
+      string name1 = "Test Vendor1";
       string description1 = "Test Vendor Description1";
       Vendor newVendor1 = new Vendor(name1, description1);
 
       string name2 = "Test Vendor2";
       string description2 = "Test Vendor Description2";
-      Vendor newVendor2 = new Vendor(name2, description2); 
+      Vendor newVendor2 = new Vendor(name2, description2);
 
       Vendor result = Vendor.Find(2);
 
       Assert.AreEqual(newVendor2, result);
     }
+
+    [TestMethod]
+      public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      string orderTitle = "Standard";
+      string orderDescription = "Standard order bread";
+      float orderPrice = 3.5F;
+      string orderDate = "01/01/2020";
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      List<Order> newList = new List<Order>{newOrder};
+
+      string vendorName = "Test Vendor";
+      string vendorDescription = "Test Vendor Description";
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Orders;
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
+}
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -33,5 +33,10 @@ namespace VendorOrderTracker.Models
     {
       return _instances[searchId-1];
     }
+
+    // public void AddOrder(Order order)
+    // {
+    //   Orders.Add(order);
+    // }
   }
 }
