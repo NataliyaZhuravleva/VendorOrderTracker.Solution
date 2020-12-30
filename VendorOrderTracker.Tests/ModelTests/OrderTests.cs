fcd47ea46e463f1a363506aa66b76d24f6e36c07
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-29 20:22:00 -0800

    Test getAll returns orders pass

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -27,5 +27,26 @@ namespace VendorOrderTracker.Tests
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      string title1 = "Standard";
+      string description1 = "Standard order bread";
+      float price1 = 3.5F;
+      string date1 = "01/01/2020";
+      Order newOrder1 = new Order(title1, description1, price1, date1);
+
+      string title2 = "Expanded";
+      string description2 = "Expanded order bread and pastry";
+      float price2 = 8.99F;
+      string date2 = "01/02/2020";
+      Order newOrder2 = new Order(title2, description2, price2, date2);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
