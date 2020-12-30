c78577f1230c12f15e9844719831102bce350d76
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-29 23:05:13 -0800

    Test create Vendor constructor pass

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTest
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor=new Vendor("test vendor", "test vendor description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public int Id { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName, string vendorDescription)
+    {
+      Name = vendorName;
+      Description = vendorDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
+  }
+}
