55b693c5fd0687771015c64aea491128e4277b50
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-30 00:23:04 -0800

    Add Show for vendors

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -19,5 +19,23 @@ namespace VendorOrderTracker.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create (string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders=selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
