fec8ce1b431e1cf1991296bde3f92d1b90e80da8
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-29 20:41:25 -0800

    Show msg if no orders in the list

---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class OrdersController:Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders=Order.GetAll();
+      return View(allOrders);
+    }
+  }
+
+}
